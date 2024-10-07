e7679c9ab8990552245ce5959df144bae7fe1128
TechnoStudy <ismettechno@gmail.com>
2024-10-07 19:53:53 +0100

    GWD hazırlık çalışması başladı

--- src/test/java/StepDefinitions/_01_LoginSteps.java
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -4,6 +4,7 @@ package StepDefinitions;
 //import io.cucumber.java.en.Then;
 //import io.cucumber.java.en.When;   //Yukarıdaki 3 impotrun yerine aşağıdaki yazılabilir
 
+import Utilities.GWD;
 import io.cucumber.java.en.*;  // Burada yıldız hepsi demek , hepsi Eklendi, import oldu
 
 public class _01_LoginSteps {
@@ -11,12 +12,17 @@ public class _01_LoginSteps {
     @Given("Navigate to Campus")
     public void navigate_to_campus() {
         System.out.println("Site açıldı");
+
+        GWD.getDriver().get("https://test.mersys.io/");
     }
 
     @When("Enter username and password and click login button")
     public void enter_username_and_password_and_click_login_button() {
-
         System.out.println("Username ve şifre gönderildi");
+
+        // driver.findElement() -> POM
+        // GWD.getDriver()
+
     }
 
     @Then("User should login successfully")
---
+++ src/test/java/Utilities/GWD.java
@@ -0,0 +1,18 @@
+package Utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GWD {  // Genel Web Driver
+    private static WebDriver driver;
+
+    public static WebDriver getDriver()
+    {
+        if (driver == null) //hiç oluşturulmamış ise
+          driver= new ChromeDriver();
+
+        // eğer zaten oluşmuşsa önceden oluşmuş driver ı gönder
+        return driver;
+    }
+
+}
