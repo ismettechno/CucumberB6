252d61b2eb090d5c198048b021dbbd52f61d70bc
TechnoStudy <ismettechno@gmail.com>
2024-10-08 19:40:42 +0100

    Create Country Senaryosu eklendi step ler eklendi

--- src/test/java/StepDefinitions/_02_CountrySteps.java
+++ src/test/java/StepDefinitions/_02_CountrySteps.java
@@ -3,16 +3,13 @@ package StepDefinitions;
 public class _02_CountrySteps {
     @io.cucumber.java.en.And("Navigate to Country")
     public void navigateToCountry() {
-
     }
 
     @io.cucumber.java.en.When("Create a country")
     public void createACountry() {
-
     }
 
     @io.cucumber.java.en.Then("Success message should be displayed")
     public void successMessageShouldBeDisplayed() {
-
     }
 }
