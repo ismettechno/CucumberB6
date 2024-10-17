a4dfe4f4efa8fa904b9bfc63b6b0c730da3180d5
TechnoStudy <ismettechno@gmail.com>
2024-10-17 18:13:56 +0100

    Extend Report adım 5: Ekran kaydı alma 2

--- src/test/java/StepDefinitions/Hooks.java
+++ src/test/java/StepDefinitions/Hooks.java
@@ -3,6 +3,8 @@ package StepDefinitions;
 import Utilities.GWD;
 import io.cucumber.java.After;
 import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {   //Kanca
 
@@ -12,9 +14,10 @@ public class Hooks {   //Kanca
     @After
     public void after(Scenario senaryo) // Cucumberın ototmarik senaryo ile ilgili bilgiler değişkeni
     {
-        if (senaryo.isFailed())
-        {
-
+        if (senaryo.isFailed()){
+            TakesScreenshot ts=((TakesScreenshot) GWD.getDriver());
+            byte[] hafizadakiHali=ts.getScreenshotAs(OutputType.BYTES);
+            senaryo.attach(hafizadakiHali, "image/png", "screenshot name");
         }
 
         GWD.quitDriver();
