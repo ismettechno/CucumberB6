0e3322954ad15da84a00105c67b5efe0aa8ae7f6
TechnoStudy <ismettechno@gmail.com>
2024-10-21 18:29:24 +0100

    _04_ApachePOIGetAllData

---
+++ src/test/java/ApachePOI/_04_ApachePOIGetAllData.java
@@ -0,0 +1,33 @@
+package ApachePOI;
+
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class _04_ApachePOIGetAllData {
+    public static void main(String[] args) throws IOException {
+        String path="src/test/java/ApachePOI/resource/ApacheExcel2.xlsx";
+        FileInputStream inputStream=new FileInputStream(path);
+        Workbook workbook= WorkbookFactory.create(inputStream);
+        Sheet sheet=workbook.getSheetAt(0); // ilk sayfa
+
+        //zoo.leng kaç satırım var
+        int rowCount= sheet.getPhysicalNumberOfRows();//satır sayısı
+        System.out.println("rowCount = " + rowCount);
+
+        for (int i = 0; i < rowCount; i++) {
+            Row row= sheet.getRow(i); // satırı aldım
+            int cellCount=row.getPhysicalNumberOfCells(); //satırdaki hücre sayisi, zoo[i] length
+
+            for (int j = 0; j < cellCount; j++) {
+                System.out.print(row.getCell(j)+" ");
+            }
+
+            System.out.println();
+        }
+
+
+    }
+}
