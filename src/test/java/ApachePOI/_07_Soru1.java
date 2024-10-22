b729d95668b428e1899dbcc3afb588cfc4e1ec3b
TechnoStudy <ismettechno@gmail.com>
2024-10-22 19:39:24 +0100

    _09_Soru  ve TODO 1 ve TODO 2

--- src/test/java/ApachePOI/_07_Soru1.java
+++ src/test/java/ApachePOI/_07_Soru1.java
@@ -1,5 +1,10 @@
 package ApachePOI;
 
+import org.apache.poi.ss.usermodel.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  *  Ekrandaki gibi bir Excel Çarpım tablosunu
  *  varolan boş bir excel dosyasına
@@ -19,4 +24,30 @@ package ApachePOI;
 //        1 x 10 = 10
 
 public class _07_Soru1 {
+    public static void main(String[] args) throws IOException {
+       String path="src/test/java/ApachePOI/resource/Carpim.xlsx";
+        FileInputStream inputStream=new FileInputStream(path);
+        Workbook workbook= WorkbookFactory.create(inputStream);
+        Sheet sheet=workbook.getSheetAt(0);
+
+        int enSonSatir=0; // sheet.getPhysicalNumberOfRows(); //kaldığın satırdan devam et
+
+        for (int i = 1; i <= 10; i++) {
+            Row yeniSatir= sheet.createRow(enSonSatir++);
+
+            Cell hucre1=yeniSatir.createCell(0);hucre1.setCellValue(1);
+            Cell hucre2=yeniSatir.createCell(1);hucre2.setCellValue("x");
+            Cell hucre3=yeniSatir.createCell(2);hucre3.setCellValue(i);
+            Cell hucre4=yeniSatir.createCell(3);hucre4.setCellValue("=");
+            Cell hucre5=yeniSatir.createCell(4);hucre5.setCellValue(i*1);
+        }
+
+        inputStream.close();
+        FileOutputStream  outputStream=new FileOutputStream(path);
+        workbook.write(outputStream);
+        workbook.close();
+        outputStream.close();
+
+        System.out.println("İşlem tamamlandı");
+    }
 }
