e345651a43c29bc0a7ab1c9e27bfe43438849b0c
TechnoStudy <ismettechno@gmail.com>
2024-10-23 19:07:29 +0100

    _11_Soru Çözüm

--- src/test/java/ApachePOI/_11_Soru.java
+++ src/test/java/ApachePOI/_11_Soru.java
@@ -10,6 +10,14 @@ package ApachePOI;
 
  */
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 public class _11_Soru {
     public static void main(String[] args) {
         String path="src/test/java/ApachePOI/resource/TestSonuclari.xlsx";
@@ -23,9 +31,39 @@ public class _11_Soru {
         writeToExcel(path, testSonuc); //dosya artık var ekleyecek
     }
 
-    writeExcel(){
-        dosya yok oluştur yaz,
-        var ise sonuna ekle
+    public static void writeToExcel(String path, String testResult){
+        File dosya=new File(path);
+        try {
+            if (!dosya.exists()) { // dosya yok ise
+                XSSFWorkbook workbook = new XSSFWorkbook(); //yeni oluştur
+                XSSFSheet sheet = workbook.createSheet("Sayfa1");
+                Row yeniSatir = sheet.createRow(0);
+                Cell yeniHucre = yeniSatir.createCell(0);
+                yeniHucre.setCellValue(testResult);  //değeri ver
+                FileOutputStream outputStream = new FileOutputStream(path);  //kaydet
+                workbook.write(outputStream);
+                workbook.close();
+                outputStream.close();
+                System.out.println("Dosya oluşturuldu");
+            } else {  //dosya var ise
+                FileInputStream inputStream = new FileInputStream(path);  //var olanı oku
+                Workbook workbook = WorkbookFactory.create(inputStream);
+                Sheet sheet = workbook.getSheetAt(0);
+                int enSonSatir = sheet.getPhysicalNumberOfRows();
+                Row yeniSatir = sheet.createRow(enSonSatir);
+                Cell yeniHucre = yeniSatir.createCell(0);
+                yeniHucre.setCellValue(testResult);  //değeri ver
+                FileOutputStream outputStream = new FileOutputStream(path);  //kaydet
+                workbook.write(outputStream);
+                workbook.close();
+                outputStream.close();
+                System.out.println("Dosyaya Eklendi");
+            }
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }
 
     }
 
