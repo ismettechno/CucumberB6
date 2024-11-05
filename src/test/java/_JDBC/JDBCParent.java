5f3781713a6e96554f262f4cd3de39d2e1d96f95
TechnoStudy <ismettechno@gmail.com>
2024-11-05 17:13:14 +0000

    JDBC_Parent_Soru2

---
+++ src/test/java/_JDBC/JDBCParent.java
@@ -0,0 +1,31 @@
+package _JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class JDBCParent {
+    private static Connection connection;
+    public static Statement statement;
+
+    public static void DBConnectionOpen() {
+        String url = "jdbc:mysql://demo.mersys.io:33906/sakila";
+        String username = "admin";
+        String password = "Techno24Study.%=";
+
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+            statement = connection.createStatement();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static void DBConnectionClose() {
+        try {
+            connection.close();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
