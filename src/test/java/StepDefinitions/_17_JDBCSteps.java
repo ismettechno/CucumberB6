a5bf6e8b16857f486002b5c31d1e9105197854f5
TechnoStudy <ismettechno@gmail.com>
2024-11-06 18:30:50 +0000

    DBUtility

---
+++ src/test/java/StepDefinitions/_17_JDBCSteps.java
@@ -0,0 +1,17 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.Then;
+
+public class _17_JDBCSteps {
+
+    @Then("Send the Query the Database {string} and control match")
+    public void sendTheQueryTheDatabaseAndControlMatch(String sorgu) {
+
+          // DB den data yı oku
+
+          // Webden(UI) dan ekrandaki isimleri oku
+
+          // birebir Assert yap
+
+    }
+}
